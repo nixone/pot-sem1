2a7a1a8835853cbbd7438d779845eadd7059d348
Martin Olesnanik <martinolesnanik@gmail.com>
2016-11-08 21:36:52 +0100

    Started working on game lib

---
+++ GameLib/AbstractIdentifiable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLib
+{
+    public abstract class AbstractIdentifiable : IIdentifiable
+    {
+        public abstract String GetId();
+
+        public override int GetHashCode()
+        {
+            return GetId().GetHashCode();
+        }
+
+        public override Boolean Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (!obj.GetType().Equals(GetType())) return false;
+            return GetId().Equals(((AbstractIdentifiable)obj).GetId());
+        }
+    }
+}
---
+++ GameLib/Game.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLib
+{
+    public abstract class Game
+    {
+        private Dictionary<String, IIdentifiable> _identifiables = new Dictionary<string, IIdentifiable>();
+
+        public IIdentifiable GetIdentifiable(String key)
+        {
+            if (!_identifiables.ContainsKey(key))
+            {
+                throw new IndexOutOfRangeException(key);
+            }
+            return _identifiables[key];
+        }
+
+        public T GetIdentifiable<T>(String key)
+        {
+            IIdentifiable identifiable = GetIdentifiable(key);
+            if (!(identifiable is T))
+            {
+                throw new IndexOutOfRangeException(key + " is not correct type");
+            }
+            return (T)identifiable;
+        }
+
+        public void Initialize()
+        {
+
+        }
+
+        public abstract int GetScore();
+    }
+}
